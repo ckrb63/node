1f49845cd50917537617cd6312c91fe3fc163cfa
cksrb63 <cksr1@naver.com>
2022-08-20 02:03:23 +0900

    'mongoose'

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,6 +8,11 @@ const {
 } = require('./../controllers/userController.js');
 const router = express.Router();
 
+router.param('id', (req, res, next, val) => {
+  console.log(`Id is ${val}`);
+  next();
+});
+
 router.route('/').get(getAllUsers).post(addUser);
 
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
--- server.js
+++ server.js
@@ -1,7 +1,54 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const app = require('./App');
+
 dotenv.config({ path: './config.env' });
 
-const app = require('./App');
+const DB = process.env.DATABASE;
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => {
+    console.log(con.connections);
+    console.log('DB connection successful!');
+  });
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The forset hiker',
+  rating: 4.7,
+  price: 497,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log('ERROR');
+  });
 
 console.log(process.env);
 
